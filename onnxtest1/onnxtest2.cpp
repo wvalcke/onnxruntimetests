f60333bf4695fd53cccda96683952684685bcac4
Wim Valcke <Wim.Valcke@be.bepco.com>
2022-12-13 13:57:45 +0100

    Fix compile issue on Windows

--- onnxtest1/onnxtest2.cpp
+++ onnxtest1/onnxtest2.cpp
@@ -8,11 +8,25 @@
 
 int main(int argc, char *argv[])
 {
-    Ort::Env env;
-    Ort::SessionOptions session_options;
-    OrtCUDAProviderOptions cuda_options;
-    cuda_options.device_id = 0;
-    session_options.AppendExecutionProvider_CUDA(cuda_options);
-    Ort::Session session = Ort::Session(env, "rim.onnx", session_options);
+    try
+    {
+        Ort::Env env;
+        Ort::SessionOptions session_options;
+        OrtCUDAProviderOptions cuda_options;
+        cuda_options.device_id = 0;
+        session_options.AppendExecutionProvider_CUDA(cuda_options);
+#ifdef _WIN32
+        Ort::Session session = Ort::Session(env, L"rim.onnx", session_options);
+#else
+        Ort::Session session = Ort::Session(env, "rim.onnx", session_options);
+#endif
+    }
+
+    catch (const Ort::Exception &exception)
+    {
+        std::cout << "ERROR running model inference: " << exception.what() << std::endl;
+        exit(-1);
+    }
+
     std::cout << "End reached" << std::endl;
 }
